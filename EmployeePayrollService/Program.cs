01ddbb6a043ea11d33bbccc9adaf3e0f8e248129
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-03-17 20:18:32 +0530

    Adding welcome message

---
+++ EmployeePayrollService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Payroll Services");
+        }
+    }
+}
